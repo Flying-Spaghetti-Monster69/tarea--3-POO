ac61299e94f4365b74a2d37fd1b5fec93ad5e3f8
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-09-18 23:52:51 -0500

    .

--- parte2/Ejercicio 22.py
+++
@@ -1,8 +0,0 @@
-nombre = input("Nombre > ")
-salario = int(input("Salario por Hora > "))
-horas = int(input("Horas Trabajadas por Mes > "))
-
-if salario*horas > 450000:
-  print(nombre, salario*horas)
-else:
-  print(nombre)
